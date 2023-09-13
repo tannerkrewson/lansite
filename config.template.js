167d1d5a9f64e42a270ce201b74bf6a4dae7da96
Marius Kiefer <marius@kekistan.info>
2023-09-13 23:40:14 +0200

    add description to new config option

--- config.template.js
+++ config.template.js
@@ -29,6 +29,7 @@ Config.privateMessaging = true;
 //Connect 4 URL: If you would prefer to host it yourself, https://github.com/tannerkrewson/connect4lansite
 Config.connectFourUrl = 'https://kevinshannon.dev/connect4/';
 
-Config.allowAllPosts = true;
+//Automatically accept all posts
+Config.allowAllPosts = false;
 
 module.exports = Config;
